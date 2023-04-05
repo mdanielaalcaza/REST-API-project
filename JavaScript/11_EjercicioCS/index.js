a9e573553a3f0f1ba54316f8e30be071d2af8306
mdanielaalcaza <94508977+mdanielaalcaza@users.noreply.github.com>
2023-04-05 17:30:09 +0200

    create header in data.csv file

--- JavaScript/11_EjercicioCS/index.js
+++ JavaScript/11_EjercicioCS/index.js
@@ -1,26 +1,37 @@
 const axios = require("axios");
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require("fs").promises;
+const path = require("path");
 
 const main = async () => {
   let response = await axios.get("https://rickandmortyapi.com/api/character");
   let {
     data: { results },
   } = response;
-  let character = results
-    .map((character) => {
-      return {
-        id: character.id,
-        name: character.name,
-        status: character.status,
-        species: character.species,
-      };
-    })
-    .map((personajes) => Object.values(personajes).join(","))
-    .join("\n");
+  let character = results.map((character) => {
+    return {
+      id: character.id,
+      name: character.name,
+      status: character.status,
+      species: character.species,
+    };
+  })
+  .map((personajes) => Object.values(personajes).join(","))
+  .join("\n");
   
-  await fs.writeFile(path.join(__dirname, 'data.csv'), character)
-    //console.log(path.join(__dirname, 'data.csv'));
-    //console.log(character);
+  let encabezados = results
+  .map((encabezado) => {
+    return {
+      id: encabezado.id,
+      name: encabezado.name,
+      status: encabezado.status,
+      species: encabezado.species,
+    };
+  })
+  let headers = Object.keys(encabezados[0]).join(',');
+  let tarea = (headers + '\n' + character);
+  await fs.writeFile(path.join(__dirname, 'data.csv'), tarea);
+  //console.log(path.join(__dirname, 'data.csv'));
+  //console.log(character);
+
 };
 main();
